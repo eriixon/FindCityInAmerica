21529d391459ad09f40e087ad6c07e65047637b5
eriixon <radkovsn@gmail.com>
2017-11-07 23:10:42 -0700

    update 171107

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
-// const config = require('config');
+const config = require('config');
 
 app.set('port', process.env.PORT||8080);
 app.use(bodyParser.urlencoded({ extended: false }));
