48d5091d32f4803b8faf497477798ca56799f5ac
eriixon <radkovsn@gmail.com>
2018-02-07 13:59:10 -0700

    update 070218

--- public/components/city.js
+++ public/components/city.js
@@ -1,12 +1,10 @@
 Vue.component('city', {
     props: ['city'],
-    template: `
-    <div>
+    template:
+    `<div>
         <div class="col-sm-6 col-md-4" style="text-align: center;">
             <div class="thumbnail">
-                <iframe width="100%" height="250" frameborder="0" style="border:0"
-                    src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB-V9QUKxnbFAnWr_9UZP6la0neIBYteZ4&q=Boise+Idaho" allowfullscreen>
-                </iframe>
+                <iframe width="100%" height="auto" frameborder="0" style="border:0" :src="city.mapLink" allowfullscreen></iframe>
                 <span class="x-delete btn-del" @click="$emit('remove', city.id)"><i class="fas fa-times"></i></span>
                 <div class="caption">
                     <h3>{{city.City}}</h3>
@@ -15,9 +13,8 @@ Vue.component('city', {
                     <p>State: {{city.State}}</p>
                     <p>Country: {{city.Country}}</p>
                     <p>
-                        <a href="#" class="btn btn-primary" role="button">Wikipedia</a>
-                        <a href="#" class="btn btn-default" role="button">City Data</a>
-                        <a href="#" class="btn btn-success" role="button">City Population</a>
+                        <a :href="city.wiki" class="btn btn-default" target="_blank"><img src="../src/wiki.png" alt="Wikipedia"></a>
+                        <a :href="city.citydata" class="btn btn-default" target="_blank"><img src="../src/cityData.png" alt="City Data"></a>
                     </p>
                 </div>
             </div>
